857daf932da24bdeb6bfd4b6855697853f35d1d4
Md Nurul Hassan <nurul@aitlindia.com>
2018-07-01 16:23:56 +0530

    new fragment added

--- app/src/main/java/com/aitl/androidapp/Main2Activity.java
+++ app/src/main/java/com/aitl/androidapp/Main2Activity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -30,6 +31,7 @@ public class Main2Activity extends AppCompatActivity {
         String name = intent2.getStringExtra("name");
 
         nameTV.setText(name);
+        Log.d("MainActivity2","onCreate Called!...");
     }
 
 
@@ -45,10 +47,47 @@ public class Main2Activity extends AppCompatActivity {
         loadFragment(BlankFragment2.newInstance("6","ABC"));
     }
 
+    public void FargmentDynamic3(View view) {
+        loadFragment(BlankFragment3.newInstance("8","XYZ"));
+    }
+
     void loadFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.container,fragment);
         fragmentTransaction.commit();
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity2","OnStart Called!...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity2","onResume Called!...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity2","onPause Called!...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","onStop Called!...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity2","onDestroy Called!...");
+    }
+
+
 }
